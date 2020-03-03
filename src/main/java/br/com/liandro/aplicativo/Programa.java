d55a51bf34b6cfaf542c324ad6df056307afb602
Dernival Liandro <liandro.sys@gmail.com>
2020-03-03 13:55:46 -0300

    Commit final

--- src/main/java/br/com/liandro/aplicativo/Programa.java
+++ src/main/java/br/com/liandro/aplicativo/Programa.java
@@ -2,16 +2,22 @@ package br.com.liandro.aplicativo;
 
 import br.com.liandro.dominio.Pessoa;
 
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+
 public class Programa {
 
     public static void main(String[] args) {
 
-        Pessoa p1 = new Pessoa(1, "Carlos da Silva", "carlos@gmail.com");
-        Pessoa p2 = new Pessoa(2, "Joaquim Torres", "joaquim@gmail.com");
-        Pessoa p3 = new Pessoa(3, "Ana Maria", "ana@gmail.com");
-        System.out.println(p1);
-        System.out.println(p2);
-        System.out.println(p3);
+        EntityManagerFactory emf = Persistence.createEntityManagerFactory("exemplo_jpa");
+        EntityManager em = emf.createEntityManager();
+
+        em.remove(new Pessoa());
+
+        System.out.println("Pronto!");
+        em.close();
+        emf.close();
 
     }
 }
